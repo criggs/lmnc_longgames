f3b2a43ebe17e7eb2012c8fe16d8b56778ce00dc
Chris Riggs <694245+criggs@users.noreply.github.com>
2023-05-14 23:12:16 -0400

    Updates to clean up logic and allow 2 AI players

--- lmnc_longpong/main.py
+++ lmnc_longpong/main.py
@@ -1,25 +1,10 @@
 import pygame
 import random
 import math
-from collections import namedtuple
 
-class Player:
-    def __init__(self, rect: pygame.Rect) -> None:
-        #Paddle
-        self.rect = rect
-        self.direction = 0
-        self.speed = 0
-        
-        #Game
-        self.ai = False
-        self.score = 0
+# Contants
+UPSCALE_FACTOR = 5
 
-# Initialize Pygame
-pygame.init()
-
-# Set up the game window
-
-UPSCALE_FACTOR = 1
 BOARD_COUNT = 22
 MATRIX_HEIGHT = 53
 MATRIX_WIDTH = 11 * BOARD_COUNT
@@ -32,6 +17,59 @@ WHITE = (255, 255, 255)
 FONT_SIZE = 8 * UPSCALE_FACTOR
 FONT_COLOR = WHITE
 
+
+# Helper Classes
+class Player:
+    def __init__(self, rect: pygame.Rect) -> None:
+        #Paddle
+        self.rect = rect
+        self.direction: int = 0 # Direction the player's paddle is moving
+        self.speed: int = 0
+        
+        #Game
+        self.is_ai: bool = False
+        self.score: int = 0
+    
+    # Side of the board the player is on
+    @property
+    def position(self):
+        if self.rect.x == 0:
+            return -1
+        return 1
+        
+    def reset(self):
+        self.rect.y = 0
+        self.speed = 0
+        self.score = 0
+        self.direction = 0
+
+class Ball:
+    def __init__(self, radius: int) -> None:
+        self.radius = radius
+        self.direction_y = -1
+        self.direction_x = 1
+        self.speed = 1
+        self.angle = 0
+        self.rect = pygame.Rect(WIDTH // 2 - self.radius // 2, HEIGHT // 2 - self.radius // 2, self.radius, self.radius)
+        
+    def reset(self):
+        self.rect.center = (WIDTH // 2, HEIGHT // 2)
+        self.angle = random.uniform(-math.pi / 4, math.pi / 4)
+        self.speed = 2 * UPSCALE_FACTOR
+    
+    @property
+    def speed_x(self):
+        return self.speed * math.cos(self.angle) * self.direction_x
+    
+    @property
+    def speed_y(self):
+        return self.speed * math.sin(self.angle) * self.direction_y
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+
 font = pygame.font.Font("lmnc_longpong/Amble-Bold.ttf", FONT_SIZE)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -43,15 +81,10 @@ ball_radius = 2 * UPSCALE_FACTOR
 paddle_width = 2 * UPSCALE_FACTOR
 paddle_height = 10 * UPSCALE_FACTOR
 
-player_paddle_speed = 2 * UPSCALE_FACTOR
-ai_paddle_speed = 1 * UPSCALE_FACTOR
+player_paddle_speed = 3 * UPSCALE_FACTOR
+ai_paddle_speed = 2 * UPSCALE_FACTOR
 
-# Init Speed Variables
-ball_speed = 1
-ball_speed_x = 1
-ball_speed_y = 1
-
-ball_max_speed = 6 * UPSCALE_FACTOR
+ball_max_speed = 10 * UPSCALE_FACTOR
 ball_min_speed = 2 * UPSCALE_FACTOR
 
 # Create paddles
@@ -59,8 +92,7 @@ player_one = Player(pygame.Rect(0, HEIGHT // 2 - paddle_height // 2, paddle_widt
 player_two = Player(pygame.Rect(WIDTH - paddle_width, HEIGHT // 2 - paddle_height // 2, paddle_width, paddle_height))
 
 # Create ball
-ball = pygame.Rect(WIDTH // 2 - ball_radius // 2, HEIGHT // 2 - ball_radius // 2, ball_radius, ball_radius)
-
+ball = Ball(2 * UPSCALE_FACTOR)
 
 # Function to update the score on the screen
 def draw_score():
@@ -69,87 +101,71 @@ def draw_score():
     text_rect = text.get_rect(center=(WIDTH // 2, FONT_SIZE))
     screen.blit(text, text_rect)
 
-# Function to update the paddles' positions
-def update_paddles():
+def update_paddle(player: Player):
+    speed = player_paddle_speed
+    if player.is_ai:
+        update_for_ai(player)
+        #Only use the slower ai speed if one player is human
+        speed = ai_paddle_speed if player_one.is_ai != player_two.is_ai else player_paddle_speed
+    player.rect.y += speed * player.direction
+    player.rect.y = max(min(player.rect.y, HEIGHT - paddle_height), 0)
+
+def update_for_ai(ai_player: Player):
+    if ball.direction_x != ai_player.position or abs(ball.rect.centerx - ai_player.rect.centerx) > WIDTH // 2:
+        ai_player.direction = 0
+        return
     
-    #TODO Change to AI speed if needed
-    player_one.rect.y += player_paddle_speed * player_one.direction
-    player_two.rect.y += player_paddle_speed * player_two.direction
-
-    # Keep the paddles inside the screen
-    player_one.rect.y = max(min(player_one.rect.y, HEIGHT - paddle_height), 0)
-    player_two.rect.y = max(min(player_two.rect.y, HEIGHT - paddle_height), 0)
-
+    # AI Player logic
+    if ai_player.rect.centery < ball.rect.centery - paddle_height // 2:
+        ai_player.direction = 1
+    elif ai_player.rect.centery > ball.rect.centery + paddle_height // 2:
+        ai_player.direction = -1
+    else:
+        ai_player.direction = 0
 
 # Function to update the ball's position
 def update_ball():
-    global ball_speed_x, ball_speed_y, player_one, player_two
+    ball.rect.x += ball.speed_x
+    ball.rect.y += ball.speed_y
 
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    
     # Detect Left Paddle Collision
-    left_collision = ball.left <= paddle_width and (abs(ball.centery - player_one.rect.centery) <= paddle_height//2)
+    left_collision = ball.rect.left <= paddle_width and (abs(ball.rect.centery - player_one.rect.centery) <= paddle_height//2)
     
     # Detect Right Paddle Collision
-    right_collision = ball.right >= WIDTH - paddle_width and (abs(ball.centery - player_two.rect.centery) <= paddle_height//2)
+    right_collision = ball.rect.right >= WIDTH - paddle_width and (abs(ball.rect.centery - player_two.rect.centery) <= paddle_height//2)
 
     # Check collision with paddles
     if left_collision:
-        delta_y = ball.centery - player_one.rect.centery
-        ball_angle = math.pi / 4 * (delta_y / (paddle_height / 2))
-        ball_speed = math.hypot(ball_speed_x, ball_speed_y)
-        ball_speed_x = ball_speed * math.cos(ball_angle)
-        ball_speed_y = ball_speed * math.sin(ball_angle)
-
-        ball_speed_x = abs(ball_speed_x)  # Ensure the ball always moves towards the AI paddle
-
-        # Increase ball speed if paddle is moving in the same direction
-        if player_one.direction * ball_speed_y > 0:
-            ball_speed_y *= 1.2
-        # Decrease ball speed if paddle is moving in the opposite direction
-        elif player_one.direction * ball_speed_y < 0:
-            ball_speed_y /= 1.2
-
-        ball_speed_y = max(min(ball_speed_y, ball_max_speed), -ball_max_speed)
-
-        # Add spin to the ball based on paddle's velocity and direction
-        ball_speed_x += player_one.speed * player_one.direction / 2
+        update_ball_speed_from_collision(player_one)
 
     elif right_collision:
-        delta_y = ball.centery - player_two.rect.centery
-        ball_angle = math.pi - math.pi / 4 * (delta_y / (paddle_height / 2))
-        ball_speed = math.hypot(ball_speed_x, ball_speed_y)
-        ball_speed_x = ball_speed * math.cos(ball_angle)
-        ball_speed_y = ball_speed * math.sin(ball_angle)
-
-        ball_speed_x = -abs(ball_speed_x)  # Ensure the ball always moves towards the player paddle
-
-        # Increase ball speed if paddle is moving in the same direction
-        if player_two.direction * ball_speed_y > 0:
-            ball_speed_y *= 1.2
-        # Decrease ball speed if paddle is moving in the opposite direction
-        elif player_two.direction * ball_speed_y < 0:
-            ball_speed_y /= 1.2
-
-        ball_speed_y = max(min(ball_speed_y, ball_max_speed), -ball_max_speed)
-
-        # Add spin to the ball based on paddle's velocity and direction
-        ball_speed_x -= player_one.speed * player_two.direction / 2
+        update_ball_speed_from_collision(player_two)
 
     # Check collision with walls
-    if ball.top <= 0 or ball.bottom >= HEIGHT:
-        ball_speed_y = -ball_speed_y
+    if ball.rect.top <= 0 or ball.rect.bottom >= HEIGHT:
+        ball.direction_y = ball.direction_y * -1
         
     # Check if the ball went out of bounds
-    if ball.right <= 0:
+    if ball.rect.right <= 0:
         player_two.score += 1
-        reset_ball()
-    elif ball.left >= WIDTH:
+        ball.reset()
+    elif ball.rect.left >= WIDTH:
         player_one.score += 1
-        reset_ball()
+        ball.reset()
 
+def update_ball_speed_from_collision(colliding_player: Player):
+    delta_y = ball.rect.centery - colliding_player.rect.centery
+    ball.angle = math.pi / 4 * (delta_y / (paddle_height / 2))
+
+    # Increase ball speed if paddle is moving in the same direction
+    if colliding_player.direction * ball.speed_y > 0:
+        ball.speed *= 1.2
+    # Decrease ball speed if paddle is moving in the opposite direction
+    elif colliding_player.direction * ball.speed_y < 0:
+        ball.speed /= 1.2
+    # Reverse the direction of travel
+    ball.direction_x = colliding_player.position * -1
+    
 # Function to display the countdown
 def display_countdown():
     global font
@@ -160,41 +176,28 @@ def display_countdown():
         pygame.display.flip()
         pygame.time.wait(1000)
 
-# Function to reset the ball's position and speed
-def reset_ball():
-    global ball_speed, ball_speed_x, ball_speed_y
+def reset_game():
+    global running, player_one, player_two, ball
+    running = True
     
-    ball.center = (WIDTH // 2, HEIGHT // 2)
-    ball_angle = random.uniform(-math.pi / 4, math.pi / 4)
-    ball_speed = 2 * UPSCALE_FACTOR
-    ball_speed_x = ball_speed * math.cos(ball_angle)
-    ball_speed_y = ball_speed * math.sin(ball_angle)
+    screen.fill(BLACK)
 
+    display_countdown()
+    ball.reset()
+    player_one.reset()
+    player_two.reset()
+    
+    player_one.is_ai = True
+    player_two.is_ai = True
 
-def update_for_ai(ai_paddle: Player, p2: bool):
-    if p2 and ball.x < WIDTH // 2 or not p2 and ball.x > WIDTH//2:
-        ai_paddle.direction = 0
-        return
-    
-    # AI Player logic
-    if ai_paddle.rect.centery < ball.centery - paddle_height // 2:
-        ai_paddle.direction = 1
-    elif ai_paddle.rect.centery > ball.centery + paddle_height // 2:
-        ai_paddle.direction = -1
-    else:
-        ai_paddle.direction = 0
-    
+reset_game()
+
 # Game loop
-running = True
-
-display_countdown()
-reset_ball()
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
+            
         # Control the player paddle
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
@@ -204,11 +207,12 @@ while running:
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 player_one.direction = 0
-
-    update_for_ai(player_two, True)
+            if event.key == pygame.K_r:
+                reset_game()
 
     # Update game elements
-    update_paddles()
+    update_paddle(player_one)
+    update_paddle(player_two)
     update_ball()
 
     # Fill the screen
