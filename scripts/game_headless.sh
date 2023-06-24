4c6809cdf1c0a206dac33abfcfd4a974c5c43903
Chris Riggs <694245+criggs@users.noreply.github.com>
2023-06-23 22:23:14 -0400

    FPS debug for all games, better keyboard breakout

--- scripts/game_headless.sh
+++ scripts/game_headless.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PYTHONPATH=$PYTHONPATH:$DIR/..
 
-python3 $DIR/../lmnc_longgames/multiverse/multiverse_game.py
+python3 $DIR/../lmnc_longgames/multiverse/multiverse_game.py "$@"
